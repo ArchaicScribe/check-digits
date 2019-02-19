5800141fb98a5220f6b0b1204a5b09559f032ccb
Alex Rauenzahn <xandermv@yahoo.com>
2019-02-19 16:34:10 -0700

    Fixed tests.

--- test/edu/cnm/deepdive/CheckTest.java
+++ test/edu/cnm/deepdive/CheckTest.java
@@ -6,17 +6,15 @@ import org.junit.jupiter.api.Test;
 
 class CheckTest {
 
+  static final int[] INPUT = {4046,6325,9788,5676,88,8925,8598,2025,4738,453};
+  static final int[] EXPECTED = {14,16,32,24,16,24,30,9,22,12};
   @Test
   void digitSum() {
-    assertEquals(4046, 14);
-    assertEquals(6325, 16);
-    assertEquals(9788, 32);
-    assertEquals(5676, 24);
-    assertEquals(88, 16);
-    assertEquals(8925,24);
-    assertEquals(8598,30);
-    assertEquals(2025,9);
-    assertEquals(4738,22);
-    assertEquals(453,12);
+   for (int i = 0; i < INPUT.length; i ++){
+     int input = INPUT[i];
+     int expected = EXPECTED[i];
+     int actual = Check.digitSum(input);
+     assertEquals(expected, actual);
+   }
   }
 }
