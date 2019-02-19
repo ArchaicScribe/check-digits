9182dcbe4e31ddcb089eb099b2e67692e4325123
Alex Rauenzahn <xandermv@yahoo.com>
2019-02-19 16:28:47 -0700

    Added tests

--- src/edu/cnm/deepdive/Check.java
+++ src/edu/cnm/deepdive/Check.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Check {
 
+  public static int digitSum(int airth) {
+    String b;
+    int c = 0;
+
+    b = Integer.toString(airth);
+    for (int i = 0; i < b.length(); i++) {
+      c += b.charAt(i) - 48;
+    }
+  return c;
+  }
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please, enter a new number: ");
@@ -14,13 +24,5 @@ public class Check {
     System.out.println(digitSum(a) / 9);
   }
 
-  public static int digitSum(int airth) {
-    String b;
-    b = Integer.toString(airth);
-    int c = 0;
-    for (int i = 0; i < b.length(); i++) {
-      c += b.charAt(i) - 48;
-    }
-  return c;
-  }
+
 }
